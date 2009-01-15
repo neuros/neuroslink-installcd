ad7f1833fda60d65799e22247a867dcb17c6678d
Tom Bruno <tom@workstation1.(none)>
2009-01-15 09:30:21 -0600

    Fix copy of configs to cd for real this time

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -6,10 +6,13 @@ cd $PRJ/configs
 ./create_seed.sh
 cp neurostvpc.seed $BUILD/preseed
 rm neurostvpc.seed
+mkdir -p $BUILD/configs
+cp sources.list $BUILD/configs
+cp ramfs-setup.sh $BUILD/configs
 
 mkisofs -r -V "Custom Ubuntu Install CD" \
-            -cache-inodes \
-            -J -l -b isolinux/isolinux.bin \
-            -c isolinux/boot.cat -no-emul-boot \
-            -boot-load-size 4 -boot-info-table \
-            -o $PRJ/$IMAGE $BUILD
+           -cache-inodes \
+           -J -l -b isolinux/isolinux.bin \
+           -c isolinux/boot.cat -no-emul-boot \
+           -boot-load-size 4 -boot-info-table \
+           -o $PRJ/$IMAGE $BUILD
